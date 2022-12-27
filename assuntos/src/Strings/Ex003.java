fdc6ed2e9228b604041f163ae5e6a7390344dd29
LucasLDM <109696278+LucasLDM@users.noreply.github.com>
2022-12-27 10:43:59 -0300

    adicionado o ex003

--- assuntos/src/Strings/Ex003.java
+++ assuntos/src/Strings/Ex003.java
@@ -1,9 +1,38 @@
+/* Escreva um programa Java para comparar duas strings lexicograficamente, ignorando as diferenças entre maiúsculas e minúsculas. */
+
 package Strings;
 
-public class Ex003 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
-    }
+public class Ex003 {
     
+    static Scanner scan = new Scanner(System.in);
+
+    public static void comparaStrings(String str1, String str2){
+
+        if(str1.compareToIgnoreCase(str2) == 0){
+            System.out.printf("%s e %s são lexicograficamente iguais.", str1, str2);
+        }
+        else if(str1.compareToIgnoreCase(str2) < 0){
+            System.out.printf("Lexicograficamente, %s é maior que %s.", str1, str2);
+        }
+        else if(str1.compareToIgnoreCase(str2) > 0){
+            System.out.printf("Lexicograficamente, %s é menor que %s.", str1, str2);
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.print("Digite um texto: ");
+        String texto1 = scan.nextLine();
+
+        System.out.print("Digite outro texto: ");
+        String texto2 = scan.nextLine();
+
+        comparaStrings(texto1, texto2);
+
+    }
+
 }
+
